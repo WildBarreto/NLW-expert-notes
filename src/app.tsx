1511f5c56c4fdc1d7ddb50422c9ea8deb2340c89
WildBarreto <wildsbsrreto@gmail.com>
2024-02-08 17:09:06 -0300

    Criar e salvar notas no localstorage

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import logo from "./assets/Logo.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem("notes");
+
+    if(notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -16,20 +45,10 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: "Hello World",
-          }}
-        />
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: "Hello World",
-          }}
-        />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
     </div>
   );
